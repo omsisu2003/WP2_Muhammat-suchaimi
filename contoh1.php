b41c5ea765157f5b0e7252c15715299b4ffa3a7d
Muhammat Suchaimi <95162175+omsisu2003@users.noreply.github.com>
2022-09-14 11:33:10 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalan </h1>";
+        echo "Nama saya muhammat suchaimi dari citayam";
+    }
+}
